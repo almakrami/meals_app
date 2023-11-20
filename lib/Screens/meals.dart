c4cf3d4bdc5219607669055c00961c3baa1b6651
Hussain <almakrami.hu@gmail.com>
2023-11-20 19:31:41 +0300

    .

--- lib/Screens/meals.dart
+++ lib/Screens/meals.dart
@@ -1,43 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/Models/meal.dart';
+import 'package:meals_app/Widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.meals, required this.title});
+  const MealsScreen({
+    super.key,
+    required this.meals,
+    required this.title,
+  });
 
   final String title;
   final List<Meal> meals;
 
   @override
   Widget build(context) {
-    Widget content = ListView.builder(
-      itemCount: meals.length,
-        itemBuilder: (ctx, index) => Text(
-              meals[index].title,
-            ));
-    if (meals.isEmpty) {
-      content = Center(
-          child: Column(
+    Widget content = Center(
+      child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-           Text('Uh oh.... nothing is here', style: Theme.of(context)
-                  .textTheme
-                  .headlineLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground)),
-          const SizedBox(
-            height: 16,
-          ),
-          Text('try selecting different thing',
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground)),
+          Text(
+            'uh oh ... nothing is here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground),
+          )
         ],
-      ));
+      ),
+    );
+
+    if (meals.isNotEmpty){
+      content = ListView.builder(itemCount:meals.length,
+      itemBuilder: (ctx, index) => MealItem(meal: meals[index],)
+
+      );
     }
+
     return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: content);
+
+      appBar: AppBar(title: Text(title),
+      ),
+      body: content,
+    );
   }
 }
